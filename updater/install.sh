741362e130de9baf1df35f8ecf11bad375e7cc56
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2024-11-27 13:47:17 +0100

    Delete install.sh

--- updater/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/printer_data/config/.volumic/updater
-chmod +x *.sh
-cd ~/klipper
-make clean KCONFIG_CONFIG=~/printer_data/config/.volumic/updater/config.ultralumic
-make KCONFIG_CONFIG=~/printer_data/config/.volumic/updater/config.ultralumic
-make KCONFIG_CONFIG=~/printer_data/config/.volumic/updater/config.ultralumic flash FLASH_DEVICE=/dev/serial/by-path/platform-5200000.usb-usb-0:1.3:1.0
