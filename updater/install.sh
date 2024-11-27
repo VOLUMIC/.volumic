84f68126ac0cc61300b080622ab8fa4059ccfb43
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2024-11-27 13:37:50 +0100

    Update install.sh

--- updater/install.sh
+++ updater/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 cd ~/printer_data/config/.volumic/updater
-sudo chmod +x *.sh
+chmod +x *.sh
+cd ~/klipper
+make clean KCONFIG_CONFIG=~/printer_data/config/.volumic/updater/config.ultralumic
+make KCONFIG_CONFIG=~/printer_data/config/.volumic/updater/config.ultralumic
+make KCONFIG_CONFIG=~/printer_data/config/.volumic/updater/config.ultralumic flash FLASH_DEVICE=/dev/serial/by-path/platform-5200000.usb-usb-0:1.3:1.0
