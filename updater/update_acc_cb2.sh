99692a82e615071a51533d12a2bc8ef7791dae48
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2024-11-29 12:39:44 +0100

    fixes

--- updater/update_acc_cb2.sh
+++ updater/update_acc_cb2.sh
@@ -5,5 +5,5 @@ cd ~/klipper
 git pull
 make clean KCONFIG_CONFIG=~/printer_data/config/.volumic/updater/config.acc
 make KCONFIG_CONFIG=~/printer_data/config/.volumic/updater/config.acc
-make KCONFIG_CONFIG=~/printer_data/config/.volumic/updater/config.acc flash FLASH_DEVICE=/dev/serial/by-path/platform-fd8c0000.usb-usb-0:1:1.0
+make KCONFIG_CONFIG=~/printer_data/config/.volumic/updater/config.acc flash FLASH_DEVICE=/dev/serial/by-path/platform-xhci-hcd.4.auto-usb-0:1.3:1.0
 sudo service klipper start
