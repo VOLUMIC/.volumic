8aae1fbe3ab7c5c1546f7af2e67d9b8ef2b9ba85
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2025-01-19 17:42:15 +0100

    Update system scripts

--- system/initlogo.sh
+++ system/initlogo.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo cp /home/Volumic/printer_data/config/.volumic/system/watermark.png /usr/share/plymouth/themes/armbian/
-sudo plymouth-set-default-theme -R armbian
 cd /home/Volumic/printer_data/config/.volumic/system/
-sudo sed -i '/^.\/initlogo.sh/s/^/#/' /home/Volumic/printer_data/config/.volumic/system/system.sh
+sudo cp watermark.png /usr/share/plymouth/themes/armbian/
+sudo plymouth-set-default-theme -R armbian
--- system/system.sh
+++ system/system.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Make updater scripts executable
 cd /home/Volumic/printer_data/config/.volumic/updater/
 sudo chmod +x *.sh
+
+# Update Mainsail images
 sudo cp /home/Volumic/printer_data/config/.volumic/system/img/icons/*.* /home/Volumic/mainsail/img/icons/
 sudo cp /home/Volumic/printer_data/config/.volumic/system/img/themes/*.* /home/Volumic/mainsail/img/themes/
 sudo cp /home/Volumic/printer_data/config/.volumic/system/img/*.* /home/Volumic/mainsail/img/
 
-#./initlogo.sh
+# Reinitialize Volumic boot logo (after a system update for exemple)
+cd /home/Volumic/printer_data/config/.volumic/system/
+#sudo cp watermark.png /usr/share/plymouth/themes/armbian/
+#sudo plymouth-set-default-theme -R armbian
+sudo sed -i '/^sudo cp watermark.png \/usr\/share\/plymouth\/themes\/armbian\//s/^/#/' system.sh
+sudo sed -i '/^sudo plymouth-set-default-theme -R armbian/s/^/#/' system.sh
