5fd4890460734f8e95a231c17c148ced30f6896f
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2025-01-19 13:14:59 +0100

    system updater

--- system/system.sh
+++ system/system.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cd /home/Volumic/printer_data/config/.volumic/system/
-sudo cp sysupdate.sh /boot/scripts/
-sudo chmod 770 /boot/scripts/sysupdate.sh
+clear
+echo "Volumic system update..."
