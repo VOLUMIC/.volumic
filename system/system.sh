7ae60d369767f208db2327daa82ae24dfde40dc9
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2025-01-19 16:03:22 +0100

    Update system.sh

--- system/system.sh
+++ system/system.sh
@@ -2,5 +2,8 @@
 
 cd /home/Volumic/printer_data/config/.volumic/updater/
 sudo chmod +x *.sh
+sudo cp /home/Volumic/printer_data/config/.volumic/system/img/icons/*.* /home/Volumic/mainsail/img/icons/
+sudo cp /home/Volumic/printer_data/config/.volumic/system/img/themes/*.* /home/Volumic/mainsail/img/themes/
+sudo cp /home/Volumic/printer_data/config/.volumic/system/img/*.* /home/Volumic/mainsail/img/
 
 #./initlogo.sh
