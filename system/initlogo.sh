99470dee8b13f90c7cff181c79da6687918cbcb1
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2025-02-24 16:10:59 +0100

    update VyperOS system

--- system/initlogo.sh
+++ system/initlogo.sh
@@ -3,3 +3,5 @@
 cd /home/Volumic/printer_data/config/.volumic/system/
 sudo cp watermark.png /usr/share/plymouth/themes/armbian/
 sudo plymouth-set-default-theme -R armbian
+#sudo sed -i '/^sudo cp watermark.png \/usr\/share\/plymouth\/themes\/armbian\//s/^/#/' system.sh
+#sudo sed -i '/^sudo plymouth-set-default-theme -R armbian/s/^/#/' system.sh
--- system/system.sh
+++ system/system.sh
@@ -10,8 +10,8 @@ sudo cp /home/Volumic/printer_data/config/.volumic/system/img/themes/*.* /home/V
 sudo cp /home/Volumic/printer_data/config/.volumic/system/img/*.* /home/Volumic/mainsail/img/
 
 # Reinitialize Volumic boot logo (after a system update for exemple)
-cd /home/Volumic/printer_data/config/.volumic/system/
-sudo cp watermark.png /usr/share/plymouth/themes/armbian/
-sudo plymouth-set-default-theme -R armbian
-sudo sed -i '/^sudo cp watermark.png \/usr\/share\/plymouth\/themes\/armbian\//s/^/#/' system.sh
-sudo sed -i '/^sudo plymouth-set-default-theme -R armbian/s/^/#/' system.sh
+#cd /home/Volumic/printer_data/config/.volumic/system/
+#sudo cp watermark.png /usr/share/plymouth/themes/armbian/
+#sudo plymouth-set-default-theme -R armbian
+#sudo sed -i '/^sudo cp watermark.png \/usr\/share\/plymouth\/themes\/armbian\//s/^/#/' system.sh
+#sudo sed -i '/^sudo plymouth-set-default-theme -R armbian/s/^/#/' system.sh
