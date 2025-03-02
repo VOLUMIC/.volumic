5955dc0495651d18fc45c175626f9010e789e21b
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2025-03-02 17:46:22 +0100

    Update vyperos_update.sh

--- system/vyperos_update.sh
+++ system/vyperos_update.sh
@@ -26,7 +26,7 @@ git pull
 cd /home/Volumic/klipper
 make clean KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc
 make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc
-make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc flash FLASH_DEVICE=/dev/serial/by-path/platform-xhci-hcd.4.auto-usb-0:1:1.0
+sudo make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc flash FLASH_DEVICE=/dev/serial/by-path/platform-xhci-hcd.4.auto-usb-0:1:1.0
 
 # Update MCU
 cd /home/Volumic/klipper
