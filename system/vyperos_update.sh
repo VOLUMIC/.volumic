65235d61e83cd7c8bd2d841e7bc0e470ff573918
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2025-03-26 14:43:07 +0100

    system update

--- system/vyperos_update.sh
+++ system/vyperos_update.sh
@@ -25,7 +25,7 @@ git pull
 # Update accelerometer MCU
 cd /home/Volumic/klipper
 make clean KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc
-make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc
+#make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc
 sudo make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc flash FLASH_DEVICE=/dev/serial/by-path/platform-xhci-hcd.4.auto-usb-0:1:1.0
 
 # Update MCU
