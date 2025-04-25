7b7b5950b8e1c8e1bec56563cae6dfe4b1539854
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2025-04-25 12:23:54 +0200

    system updates

--- updater/update_acc_cb2.sh
+++ updater/update_acc_cb2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/klipper
+cd /home/Volumic/klipper
 make clean KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc
 make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc
-make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc flash FLASH_DEVICE=/dev/serial/by-path/platform-xhci-hcd.4.auto-usb-0:1:1.0
+sudo make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc flash FLASH_DEVICE=/dev/serial/by-path/platform-xhci-hcd.4.auto-usb-0:1.3:1.0
--- updater/update_manta_cb2.sh
+++ updater/update_manta_cb2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ~/klipper
+cd /home/Volumic/klipper
 make clean KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.manta
 make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.manta
-make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.manta flash FLASH_DEVICE=/dev/serial/by-path/platform-xhci-hcd.4.auto-usb-0:1.4:1.0
+cd /home/Volumic/klipper/lib/hidflash
+./hid-flash /home/Volumic/klipper/out/klipper.bin serial/by-path/platform-xhci-hcd.4.auto-usb-0:1.4:1.0
