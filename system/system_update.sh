e7feade5888c2bce5c8ead3ca3146ef2780688c8
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2025-09-09 18:03:24 +0200

    Update system_update.sh

--- system/system_update.sh
+++ system/system_update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# update backport repository with archive (end of support on 2024)
+sudo sed -i 's/deb.debian.org\/debian bullseye-backports/archive.debian.org\/debian bullseye-backports/' /etc/apt/sources.list
+
 # Update system
 sudo apt update
 sudo apt upgrade -y
