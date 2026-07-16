7130c32ebb934d1d1d0dab5c649ba0ba8b229682
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2026-07-16 15:22:51 +0200

    Update resetnetwork.sh

--- system/resetnetwork.sh
+++ system/resetnetwork.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cd /home/Volumic/printer_data/config/.volumic/system/
-sudo cp -f interfaces /etc/network/
-sudo cp -f interfaces /etc/network/interfaces.default
+hexchars="0123456789abcdef"
+end=$( for i in {1..6} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
+sudo cp /home/Volumic/printer_data/config/.volumic/system/interfaces /etc/network/interfaces
+sudo cp /home/Volumic/printer_data/config/.volumic/system/interfaces /etc/network/interfaces.default
+sudo sed -i "s/:00:00:00/$end/g" /etc/network/interfaces.default
+sudo sed -i "s/:00:00:00/$end/g" /etc/network/interfaces
+sudo reboot
