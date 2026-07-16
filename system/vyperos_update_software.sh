fb7147b254ad8916df88d914927a211fef087613
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2026-07-16 18:22:58 +0200

    Update vyperos_update_software.sh

--- system/vyperos_update_software.sh
+++ system/vyperos_update_software.sh
@@ -1,42 +1,59 @@
 #!/bin/bash
 
+sudo systemctl stop KlipperScreen 2>/dev/null || true
+sudo plymouth quit --retain-splash 2>/dev/null || true
+sudo sh -c 'echo "" > /dev/tty1'
+sudo sh -c 'echo "" > /dev/tty1'
+sudo sh -c 'printf "\033[2J\033[H" > /dev/tty1'
+sudo sh -c 'echo "" > /dev/tty1'
+sudo sh -c 'echo "   MISE A JOUR VOLUMIC, VEUILLEZ PATIENTER..." > /dev/tty1'
+sudo sh -c 'echo " " > /dev/tty1'
+
 ping -q -c 2 www.google.fr >/dev/null 2>&1	# test if internet is connected
 
 if [ $? -eq 0 ]; then	# internet connected
 
+  sudo sh -c 'echo "-> Mise a jour Internet, arret des services VyperOS" > /dev/tty1'
 	sudo service klipper stop
 
 	# Update KlipperScreen
+  sudo sh -c 'echo "-> Mise a jour Affichage" > /dev/tty1'
 	cd /home/Volumic/KlipperScreen
 	git reset --hard
 	git clean -fd
 	git pull
 
 	# Update mainsail
+  sudo sh -c 'echo "-> Mise a jour serveur Web" > /dev/tty1'
 	cd /home/Volumic/mainsail
 	rm -R -f ./*
 	rm .version
 	wget -q -O mainsail.zip https://github.com/mainsail-crew/mainsail/releases/latest/download/mainsail.zip && unzip mainsail.zip && rm mainsail.zip
 
 	# Update klipper
+  sudo sh -c 'echo "-> Mise a jour noyau" > /dev/tty1'
 	cd /home/Volumic/klipper
 	git pull
 
 	# Update Moonraker
+  sudo sh -c 'echo "-> Mise a jour API" > /dev/tty1'
 	cd /home/Volumic/moonraker
 	git pull
 
 	# Update Obico
+  sudo sh -c 'echo "-> Mise a jour IA" > /dev/tty1'
 	cd /home/Volumic/moonraker-obico/
 	git pull
 
 	# Update accelerometer MCU
+  sudo sh -c 'echo "-> Mise a jour firmware accelerometre" > /dev/tty1'
 	cd /home/Volumic/klipper
 	make clean KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc
 	make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc
 	sudo make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc flash FLASH_DEVICE=/dev/serial/by-path/platform-xhci-hcd.4.auto-usb-0:1:1.0
 
 	# Update MCU
+  sudo sh -c 'echo "-> Mise a jour firmware MCU" > /dev/tty1'
 	cd /home/Volumic/VyperOS
 	if [ -d "SAM3X8E" ]; then
 		cd /home/Volumic/klipper
@@ -58,6 +75,7 @@ if [ $? -eq 0 ]; then	# internet connected
 	fi
 
 	# Force system update after boot
+  sudo sh -c 'echo "-> Mise a jour systeme" > /dev/tty1'
 	cd /home/Volumic/VyperOS
 	if [ -d "sys1" ]; then
 		rmdir sys1
@@ -65,20 +83,19 @@ if [ $? -eq 0 ]; then	# internet connected
 
 else	# no internet connexion
 
-	echo "Local update"
+  sudo sh -c 'echo "-> Mise a jour locale (firmware only)" > /dev/tty1'
+  sudo sh -c 'echo "-> Arret des services" > /dev/tty1'
 	sudo service klipper stop
 
-	# Update USB Loader
-	sudo ln -sf ~/Moonraker-loader/assets/89-moonraker-loader.rules /etc/udev/rules.d
-	sudo ln -sf ~/Moonraker-loader/assets/*.sh /usr/local/sbin
-
 	# Update accelerometer MCU
+  sudo sh -c 'echo "-> Mise a jour accelerometre" > /dev/tty1'
 	cd /home/Volumic/klipper
 	make clean KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc
 	make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc
 	sudo make KCONFIG_CONFIG=/home/Volumic/VyperOS/updater/config.acc flash FLASH_DEVICE=/dev/serial/by-path/platform-xhci-hcd.4.auto-usb-0:1:1.0
 
 	# Update MCU
+  sudo sh -c 'echo "-> Mise a jour MCU" > /dev/tty1'
 	cd /home/Volumic/VyperOS
 	if [ -d "SAM3X8E" ]; then
 		cd /home/Volumic/klipper
@@ -100,12 +117,9 @@ else	# no internet connexion
 
 fi
 
-# Update Mainsail images
 sudo cp -f /home/Volumic/printer_data/config/.volumic/system/img/icons/*.* /home/Volumic/mainsail/img/icons/
 sudo cp -f /home/Volumic/printer_data/config/.volumic/system/img/themes/*.* /home/Volumic/mainsail/img/themes/
 sudo cp -f /home/Volumic/printer_data/config/.volumic/system/img/*.* /home/Volumic/mainsail/img/
-
-# Update configs
 sudo cp -f /home/Volumic/printer_data/config/.volumic/system/KlipperScreen.conf /home/Volumic/printer_data/config
 sudo cp -f /home/Volumic/printer_data/config/.volumic/system/mainsail_style.css /home/Volumic/printer_data/config/.theme/custom.css
 
