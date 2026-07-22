2a8933ac044855f1b958dabd97978f57911eca8a
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2026-07-22 17:32:51 +0200

    Update system_update.sh

--- system/system_update.sh
+++ system/system_update.sh
@@ -16,6 +16,14 @@ sudo apt upgrade -y
 sudo cp -f /home/Volumic/printer_data/config/.volumic/system/watermark.png /usr/share/plymouth/themes/armbian/
 sudo plymouth-set-default-theme -R armbian
 
+cd /home/Volumic/VyperOS
+cp -u -f /home/Volumic/printer_data/config/.volumic/updater/*.* updater
+cp -f /home/Volumic/printer_data/config/.volumic/system/*.sh /home/Volumic/VyperOS
+cp -f /home/Volumic/printer_data/config/.volumic/system/KlipperScreen.conf /home/Volumic/printer_data/config/KlipperScreen.conf
+sudo chmod 776 updater/*.sh
+sudo chmod 776 *.sh
+sudo cp -f /home/Volumic/printer_data/config/.volumic/system/90-usb.rules /etc/udev/rules.d/90-usb.rules
+sudo cp /home/Volumic/printer_data/config/.volumic/system/vyper-usb /etc/sudoers.d/vyper-usb
 cd /home/Volumic/
 if [ -d "Moonraker-loader" ]; then
 	sudo mv /home/Volumic/Moonraker-loader /home/Volumic/Moonraker-loader.old
