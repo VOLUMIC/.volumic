f8aa80fcd77008e5dc518221bbf85ffe36294520
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2026-07-23 17:48:38 +0200

    Update system.sh

--- system/system.sh
+++ system/system.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+{
 
 # Check VyperOS dir
 cd /home/Volumic
@@ -10,11 +11,18 @@ fi
 cd /home/Volumic/VyperOS
 if [ ! -d "updater" ]; then
 	mkdir updater
-	sudo chmod -R -v 776 SecureMemory
+fi
+if [ ! -d "check1" ]; then
+	mkdir check1
 fi
 
+sudo cp -f /home/Volumic/printer_data/config/.volumic/system/90-usb.rules /etc/udev/rules.d/90-usb.rules
+sudo cp /home/Volumic/printer_data/config/.volumic/system/vyper-usb /etc/sudoers.d/vyper-usb
 cp -u -f /home/Volumic/printer_data/config/.volumic/updater/*.* updater
-sudo chmod +x updater/*.sh
+cp -f /home/Volumic/printer_data/config/.volumic/system/*.sh /home/Volumic/VyperOS
+cp -f /home/Volumic/printer_data/config/.volumic/system/KlipperScreen.conf /home/Volumic/printer_data/config/KlipperScreen.conf
+sudo chmod +x /home/Volumic/VyperOS/updater/*.sh
+sudo chmod +x /home/Volumic/VyperOS/*.sh
 sudo chmod 666 /home/Volumic/printer_data/database/moonraker-sql.db
 
 # One time system update
@@ -22,9 +30,7 @@ cd /home/Volumic/VyperOS
 sudo chmod +x *.sh
 if [ ! -d "sys1" ]; then
 	mkdir sys1
-	cp -f /home/Volumic/printer_data/config/.volumic/system/system_update.sh /home/Volumic/VyperOS
-	cp -f /home/Volumic/printer_data/config/.volumic/system/vyperos_update.sh /home/Volumic/VyperOS
-	cp -f /home/Volumic/printer_data/config/.volumic/system/vyperos_update_software.sh /home/Volumic/VyperOS
-	sudo chmod +x *.sh
-	./system_update.sh > lastsysupdate.log
+	/home/Volumic/VyperOS/system_update.sh > lastsysupdate.log
 fi
+
+}
