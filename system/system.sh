e88b993ee2484b08b9dcecb3650a714e1f1983f6
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2026-07-23 16:29:27 +0200

    Update system.sh

--- system/system.sh
+++ system/system.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+{
 
 # Check VyperOS dir
 cd /home/Volumic
@@ -22,9 +23,15 @@ cd /home/Volumic/VyperOS
 sudo chmod +x *.sh
 if [ ! -d "sys1" ]; then
 	mkdir sys1
-	cp -f /home/Volumic/printer_data/config/.volumic/system/system_update.sh /home/Volumic/VyperOS
-	cp -f /home/Volumic/printer_data/config/.volumic/system/vyperos_update.sh /home/Volumic/VyperOS
-	cp -f /home/Volumic/printer_data/config/.volumic/system/vyperos_update_software.sh /home/Volumic/VyperOS
-	sudo chmod +x *.sh
+	sudo cp -f /home/Volumic/printer_data/config/.volumic/system/90-usb.rules /etc/udev/rules.d/90-usb.rules
+	sudo cp /home/Volumic/printer_data/config/.volumic/system/vyper-usb /etc/sudoers.d/vyper-usb
+	cd /home/Volumic/VyperOS
+	cp -u -f /home/Volumic/printer_data/config/.volumic/updater/*.* updater
+	cp -f /home/Volumic/printer_data/config/.volumic/system/*.sh /home/Volumic/VyperOS
+	cp -f /home/Volumic/printer_data/config/.volumic/system/KlipperScreen.conf /home/Volumic/printer_data/config/KlipperScreen.conf
+	sudo chmod 776 updater/*.sh
+	sudo chmod 776 *.sh
 	/home/Volumic/VyperOS/system_update.sh > lastsysupdate.log
 fi
+
+}
