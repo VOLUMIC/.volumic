0f1dc4b4b33651fc78526c1fb0dccf2b6e22ea3d
VOLUMIC <31067164+VOLUMIC@users.noreply.github.com>
2026-07-27 17:03:00 +0200

    Update system_update.sh

--- system/system_update.sh
+++ system/system_update.sh
@@ -8,6 +8,7 @@ sudo cp -f /home/Volumic/printer_data/config/.volumic/system/90-usb.rules /etc/u
 cp -u -f /home/Volumic/printer_data/config/.volumic/updater/*.* /home/Volumic/VyperOS/updater
 cp -f /home/Volumic/printer_data/config/.volumic/system/*.sh /home/Volumic/VyperOS
 cp -f /home/Volumic/printer_data/config/.volumic/system/KlipperScreen.conf /home/Volumic/printer_data/config/KlipperScreen.conf
+cp -f /home/Volumic/printer_data/config/.volumic/system/moonraker.env /home/Volumic/printer_data/systemd/moonraker.env
 sudo chmod +x /home/Volumic/VyperOS/updater/*.sh
 sudo chmod +x /home/Volumic/VyperOS/*.sh
 sudo chmod 666 /home/Volumic/printer_data/database/moonraker-sql.db
